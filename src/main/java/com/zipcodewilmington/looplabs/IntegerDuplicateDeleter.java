d7d182779111119bcc3d3019b101bf7bcf803fa1
Reeseyverse <jorrisecko@gmail.com>
2024-04-15 07:32:00 -0400

    finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,50 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(Integer num : array){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> results = new ArrayList<>();
+
+        for(Integer num : array){
+            if(freqMap.get(num) < maxNumberOfDuplications){
+                results.add(num);
+            }
+        }
+        return results.toArray(new Integer[0]);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(Integer num : array){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> results = new ArrayList<>();
+
+        for(Integer num : array){
+            if(freqMap.get(num) != exactNumberOfDuplications){
+                results.add(num);
+            }
+        }
+        return results.toArray(new Integer[0]);
+    }
+
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,50 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        Map<String, Integer> freqMap = new HashMap<>();
+        for(String string : array){
+            freqMap.put(string, freqMap.getOrDefault(string, 0) + 1);
+        }
+
+        List<String> results = new ArrayList<>();
+
+        for(String string : array){
+            if(freqMap.get(string) < maxNumberOfDuplications){
+                results.add(string);
+            }
+        }
+        return results.toArray(new String[0]);
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Map<String, Integer> freqMap = new HashMap<>();
+        for(String string : array){
+            freqMap.put(string, freqMap.getOrDefault(string, 0) + 1);
+        }
+
+        List<String> results = new ArrayList<>();
+
+        for(String string : array){
+            if(freqMap.get(string) != exactNumberOfDuplications){
+                results.add(string);
+            }
+        }
+        return results.toArray(new String[0]);
+    }
+
 }
